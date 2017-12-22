015dc57c8f9139efaec7cf3f8d97fae70460e5c0
ydeydier <ydeydier@users.noreply.github.com>
2017-12-22 11:24:19 +0100

    ldap1

--- classes/class.ldap.php
+++ classes/class.ldap.php
@@ -1,7 +1,44 @@
 <?php
 class ldap {
 	static function verifierLoginPassword($login, $password, $tConnexionLDAP) {
-		// TODO: finir !
+
+	/*
+
+    $adServer = "192.168.202.2";
+    $ldap = ldap_connect($adServer);
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $ldaprdn = 'SJDB' . "\\" . $username;
+
+    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+
+    $bind = @ldap_bind($ldap, $ldaprdn, $password);
+
+
+    if ($bind) {
+        $filter="(sAMAccountName=$username)";
+        $result = ldap_search($ldap,"dc=SJDB,dc=LOCAL",$filter);
+        ldap_sort($ldap,$result,"sn");
+        $info = ldap_get_entries($ldap, $result);
+        for ($i=0; $i<$info["count"]; $i++)
+        {
+            if($info['count'] > 1)
+                break;
+            echo "<p>You are accessing <strong> ". $info[$i]["sn"][0] .", " . $info[$i]["givenname"][0] ."</strong><br /> (" . $info[$i]["samaccountname"][0] .")</p>\n";
+            echo '<pre>';
+            var_dump($info);
+            echo '</pre>';
+            $userDn = $info[$i]["distinguishedname"][0]; 
+        }
+        @ldap_close($ldap);
+    } else {
+        $msg = "Invalid email address / password";
+        echo $msg;
+    }
+	*/
+
 		return true;
 	}
 }
